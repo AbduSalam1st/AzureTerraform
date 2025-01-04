40fd836359dc3208853f59bd2135c8d9d0e25b18
Abdul Salam <absm510@gmail.com>
2025-01-04 17:59:06 +0000

    Changes made to make sentinel in terraform

--- Project 1/main.tf
+++ Project 1/main.tf
@@ -67,15 +67,63 @@ resource "azurerm_subnet" "AGWSub" {
   address_prefixes     = ["192.168.1.0/24"]
 }
 
-# output "VPCinfo_details" {
-#   value       =  module.networking.VPCinfo_output
-#   description = "Details of the created Virtual Network"
-# }
+variable "vm_name" {
+  description = "Name of the virtual machine to manage based on Sentinel alert"
+  type        = string
+}
 
-# output "subnetInfo_details" {
-#   value       = module.networking.subnetInfo_output
-#   description = "Details of the created Subnets"
-# }
+resource "azurerm_network_interface" "VM_network_interface" {
+  name                =  "NIC"
+  location            = var.resoruce_location
+  resource_group_name = var.resource_group_name
+
+  ip_configuration {
+    name                          = "testconfiguration1"
+    subnet_id                     = azurerm_subnet.Subnet1.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_virtual_machine" "Workstation1" {
+  name                  = azurerm_virtual_machine.Workstation1.id
+  location              = var.resoruce_location
+  resource_group_name   = var.resource_group_name
+  network_interface_ids = [azurerm_network_interface.VM_network_interface.id]
+  vm_size               = "Standard_DS1_v2"
+
+  # Uncomment this line to delete the OS disk automatically when deleting the VM
+  # delete_os_disk_on_termination = true
+
+  # Uncomment this line to delete the data disks automatically when deleting the VM
+  # delete_data_disks_on_termination = true
+
+  storage_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+  storage_os_disk {
+    name              = "myosdisk1"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+  os_profile {
+    computer_name  = var.computer_name
+    admin_username = var.admin_username
+    admin_password = var.admin_password
+  }
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+  tags = {
+    environment = "testing"
+  }
+}
+
+
+
 
 resource "azurerm_kubernetes_cluster" "cluster" {
   name                = "learnk8scluster"
@@ -204,9 +252,7 @@ resource "azurerm_role_assignment" "agic_network_contributor" {
 
 # module "security" {
 # Terraform Configuration for Azure Sentinel Automation Sandboxing Infrastructure
-variable "vm_name" {
-  default = "testingSentinelVM"
-}
+
 
 
 resource "azurerm_resource_group" "sandbox" {
