e2731ba9cfe2c29bc58b29453e60ad72caa35020
Abdul Salam <absm510@gmail.com>
2025-05-10 17:43:51 +0100

    Pulumi implementations using a hybrid approach for more functionality of the cloud

--- Project 1/modules/AGIC/outputs.tf
+++ Project 1/modules/AGIC/outputs.tf
@@ -1,9 +1,9 @@
-output "appgw_id" {
-  description = "ID of the Application Gateway"
-  value       = azurerm_application_gateway.this.id
-}
+# output "appgw_id" {
+#   description = "ID of the Application Gateway"
+#   value       = azurerm_application_gateway.this.id
+# }
 
-output "appgw_identity_id" {
-  description = "User Assigned Identity ID for Application Gateway"
-  value       = azurerm_user_assigned_identity.appgw_identity.id
-}
+# output "appgw_identity_id" {
+#   description = "User Assigned Identity ID for Application Gateway"
+#   value       = azurerm_user_assigned_identity.appgw_identity.id
+# }
